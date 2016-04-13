282f7737878a1c6eae2f5906af00024ba923a39a
jkuri <jkuri88@gmail.com>
2016-04-13 17:50:39 +0200

    fixes

--- index.js
+++ index.js
@@ -4,13 +4,27 @@
 let config = require('./lib/config');
 
 config.object1.object2.value = 'Hello World!';
-//config.object1.object2.value.push('ola');
 config.object1.object2.array.push('Hello World!');
 config.object1.object2.array.push('Hello World 2!');
-config.object1.object2.array.reverse();
 
 console.log(JSON.stringify(config, null, 2));
 
-//console.log(config.object1.object2.array);
-
-//console.log(config.object1.object2.array[0]); // 'Hello World 2!'
+// {
+//   "object1": {
+//     "type": "object",
+//     "object2": {
+//       "type": "object",
+//       "value": {
+//         "type": "string",
+//         "value": "Hello World!"
+//       },
+//       "array": {
+//         "type": "array",
+//         "enum": [
+//           "Hello World!",
+//           "Hello World 2!"
+//         ]
+//       }
+//     }
+//   }
+// }
